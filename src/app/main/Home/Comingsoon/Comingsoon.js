61f4264af49f0322d73a9ccca631a0cb96e090fb
mujtaba45 <mujtaba.dev.techinoid@gmail.com>
2022-05-27 18:07:49 +0500

    comingsoon ui animation and some code fixing

--- src/app/main/Home/Comingsoon/Comingsoon.js
+++ src/app/main/Home/Comingsoon/Comingsoon.js
@@ -14,8 +14,24 @@ import TextField from "@mui/material/TextField";
 import { motion } from 'framer-motion';
 
 export default function () {
+
+  let boxVariants = {};
+  const isMobile = window.innerWidth < 600;
+  if (!isMobile) {
+    boxVariants = {
+      hidden: { y: -100, opacity: 0 },
+      visible: { y: 0, opacity: 1, transition: { delay: 1, duration: 1.5 } }
+    };
+  }
+  else {
+    boxVariants = {
+      hidden: { y: 100, opacity: 0 },
+      visible: { y: 0, opacity: 1, transition: { delay: 1, duration: 1.5 } }
+    };
+  }
+
   return (
-    <div>
+    <>
       <div className="flex flex-row justify-center mt-32">
         <motion.img src="assets/images/logos/ComingsoonLogo.svg" alt="logo"
           initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1, transition: { delay: 1, duration: 1.5 } }}
@@ -25,12 +41,12 @@ export default function () {
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, transition: { delay: 1, duration: 1.5 } }}
-        className="pb-96 sm:pb-128"
+        className="h-640"
         style={{ backgroundImage: "url('/assets/images/backgrounds/bgimg.jpg')" }}
       >
         <div className="sm:flex justify-between mt-68">
-          <motion.div initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1, transition: { delay: 1, duration: 1.5 } }} className="mx-20 sm:mx-96">
-            <div className="text-center sm:text-left -mx-20 sm:mx-auto">
+          <div className="mx-20 sm:mx-96">
+            <motion.div initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1, transition: { delay: 1, duration: 1.5 } }} className="text-center sm:text-left -mx-20 sm:mx-auto">
               <Typography
                 className="font-normal text-base"
                 style={{ color: "#98A2B3" }}
@@ -38,7 +54,7 @@ export default function () {
                 Coming Soon
               </Typography>
               <Typography
-                className="text-48 sm:text-64 font-medium mt-8"
+                className="text-48 sm:text-6xl font-medium mt-8"
                 style={{ color: "#000000" }}
               >
                 Easy Savari<br></br>Manzil asaan..
@@ -52,8 +68,42 @@ export default function () {
               <Typography className="font-300 text-sm sm:text-base" style={{ color: "#667085" }}>
                 coming your way to make your travel seamless.
               </Typography>
-            </div>
+            </motion.div>
 
+            <motion.div variants={boxVariants} initial="hidden" animate="visible" className="absolute sm:static bottom-96 sm:bottom-auto w-full sm:w-auto pr-40 sm:pr-0">
+              <Typography className="font-medium text-xl sm:mt-44 mb-6 sm:mr-0 mr-8">
+                Get notified when we launch!
+              </Typography>
+              <div className="flex items-center mb-4 h-40 sm:h-52 bg-white"
+                style={{ borderRadius: '30px', border: '1px solid #E6E9EA' }}>
+                <TextField
+                  fullWidth
+                  variant="standard"
+                  InputProps={{ disableUnderline: true }}
+                  className="pl-16"
+                  placeholder="Your email address"
+                />
+              </div>
+              <Typography
+                className="ml-10 mb-16 text-xs font-normal"
+                style={{ color: "#667085" }}
+              >
+                Don't worry, we won't spam you.
+              </Typography>
+
+              <Button fullWidth variant="contained"
+                className="h-40 sm:h-52 rounded-full text-12 sm:text-16 font-normal"
+                style={{ backgroundColor: '#D22A8F' }}
+              >
+                Notify me
+              </Button>
+            </motion.div>
+          </div>
+
+          {/* <motion.div
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 500, opacity: 1, transition: { delay: 1, duration: 1.5 } }}
+          >
             <div className="absolute sm:static bottom-96 sm:bottom-auto w-full sm:w-auto pr-40 sm:pr-0">
               <Typography className="font-medium text-xl sm:mt-44 mb-6 sm:mr-0 mr-8">
                 Get notified when we launch!
@@ -82,14 +132,14 @@ export default function () {
                 Notify me
               </Button>
             </div>
-          </motion.div>
+          </motion.div> */}
 
           <div className="mt-60 sm:mt-20 mb-20 sm:mb-auto">
             <motion.img initial={{ x: 1000 }} animate={{ x: 0, transition: { delay: 1, duration: 1.5 } }} className="w-auto sm:h-400 sm:absolute sm:top-80 sm:left-512 md:absolute md:top-136 md:left-lg" style={{}} src="assets/images/GariConnect/pinkcar.png" alt="logo" />
           </div>
 
           <div
-            className="h-48 w-full flex justify-center"
+            className="h-48 sm:h-80 w-full flex justify-center"
             style={{ backgroundColor: "#FFFFFF", position: "absolute", bottom: "0" }}
           >
             <Typography
@@ -101,6 +151,6 @@ export default function () {
           </div>
         </div>
       </motion.div>
-    </div>
+    </>
   );
 }
--- src/app/main/Host/HostConfig.js
+++ src/app/main/Host/HostConfig.js
@@ -2,7 +2,6 @@ import HostTabsConfig from "./hostPanel/Tabs/HostTabsConfig";
 import MenuConfig from "./menu/MenuConfig";
 import SharedConfig from "./sharedComponents/SharedConfig"
 import CarsListingConfig from "./carListing.js/CarsListingConfig";
-import RecieptConfig from "./sharedComponents/RecieptConfig";
 
 const HostConfig = [
     HostTabsConfig,
@@ -10,7 +9,6 @@ const HostConfig = [
     SharedConfig,
     HostTabsConfig,
     CarsListingConfig,
-    RecieptConfig
 ];
 
 export default HostConfig;
