4796ac99b6d472609eb13951d927e87b6eede8a5
mujtaba45 <mujtaba.dev.techinoid@gmail.com>
2022-07-18 19:02:26 +0500

    comingsoon responsive ui

--- src/app/main/Home/Comingsoon/Comingsoon.js
+++ src/app/main/Home/Comingsoon/Comingsoon.js
@@ -22,7 +22,7 @@ export default function () {
   }
 
   return (
-    <>
+    <div className="h-auto">
       <div className="flex flex-row justify-center mt-32">
         <motion.img src="assets/images/logos/ComingsoonLogo.svg" alt="logo"
           initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1, transition: { delay: 1, duration: 1.5 } }}
@@ -35,23 +35,23 @@ export default function () {
         className="h-640"
         style={{ backgroundImage: "url('/assets/images/backgrounds/bgimg.jpg')" }}
       >
-        <div className="sm:flex justify-between sm:mt-68 mt-20">
+        <div className="sm:flex justify-between sm:mt-68 mt-40">
           <div className="mx-20 sm:mx-96">
             <motion.div initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1, transition: { delay: 1, duration: 1.5 } }} className="text-center sm:text-left -mx-20 sm:mx-auto">
               <Typography
-                className="font-normal text-base"
+                className="font-normal text-base mb-20 sm:mb-0"
                 style={{ color: "#98A2B3" }}
               >
                 Coming Soon
               </Typography>
               <Typography
-                className="text-48 sm:text-6xl text-3xl font-medium mt-8"
+                className="sm:text-6xl text-4xl font-medium mt-8"
                 style={{ color: "#000000" }}
               >
                 Easy Savari<br></br>Manzil asaan..
               </Typography>
               <Typography
-                className="font-300 text-sm sm:text-base mt-12"
+                className="font-300 text-sm sm:text-base mt-32 sm:mt-12"
                 style={{ color: "#667085" }}
               >
                 Stay tuned for an exciting and convenient service
@@ -61,7 +61,7 @@ export default function () {
               </Typography>
             </motion.div>
 
-            <motion.div variants={boxVariants} initial="hidden" animate="visible" className="absolute sm:static bottom-60 sm:bottom-auto w-full sm:w-auto pr-40 sm:pr-0">
+            <motion.div variants={boxVariants} initial="hidden" animate="visible" className="absolute sm:static bottom-128 sm:bottom-auto w-full sm:w-auto pr-40 sm:pr-0">
               <Typography className="font-medium text-xl sm:mt-44 mb-6 sm:mr-0 mr-8">
                 Get notified when we launch!
               </Typography>
@@ -108,6 +108,6 @@ export default function () {
           </div>
         </div>
       </motion.div>
-    </>
+    </div>
   );
 }
